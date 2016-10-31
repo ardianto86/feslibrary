ebde7a365787e58b0228d26678518f78cb5f98f9
Ardianto Suhendar <ardianto86@gmail.com>
2016-10-31 09:05:37 +0800

    Initial commit

---
+++ FesLibrary/FesLibrary/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FesLibrary.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Catalogue()
+        {
+            ViewBag.Message = "Catalogue";
+            return View();
+        }
+
+        public ActionResult Borrow()
+        {
+            ViewBag.Message = "Borrow";
+
+            return View();
+        }
+
+        public ActionResult Extend()
+        {
+            ViewBag.Message = "Extend";
+
+            return View();
+        }
+
+        public ActionResult Return()
+        {
+            ViewBag.Message = "Return";
+
+            return View();
+        }
+    }
+}
---
+++ FesLibrary/FesLibrary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FesLibrary.Startup))]
+namespace FesLibrary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
